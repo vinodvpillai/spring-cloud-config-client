8fbfa5144c2f9826c0498519d0a1519a335dc5a3
Vinod Pillai <vinodthebest@gmail.com>
2020-09-28 22:56:17 +0530

    spring-config-client: Added actuator for get updated property values
    - Added the dependency
    - Added refresh scope in the class for using property values.
    - Updated the property value for the actuator.

--- src/main/java/com/vinod/config/client/controller/UserController.java
+++ src/main/java/com/vinod/config/client/controller/UserController.java
@@ -3,12 +3,19 @@ package com.vinod.config.client.controller;
 import com.vinod.config.client.config.PaymentGatewayProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class UserController {
 
+    /**
+     * Note: To get updated property need to call actuator refresh endpoint.
+     *
+     * POST localhost:8080/actuator/refresh
+     */
     @Value("${db.connectionUrl}")
     private String dbConnectionUrl;
 
